d9caa477f1ceed67055fa302c8a7e30cd6eb7d20
Dima <shedi26@mail.ru>
2016-03-14 20:33:47 +0300

    HW2 Task D

--- HW2/D.cpp
+++ HW2/D.cpp
@@ -4,3 +4,54 @@
 	Входные данные
 В первой строке дано число N ≤ 106. Далее следует N строк с координатами, разделёнными пробелом. Каждая координата имеет не более двух знаков после запятой.
 */
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main () {
+	int N;
+	cin >> N;
+	
+	float X[N], Y[N];
+	
+	for (int i=0; i<N; i++) {
+		cin >> X[i];
+		cin >> Y[i];
+	}
+	
+	float rminX=0, rmaxX=0, rY=0; //right semiarea
+	float lminX=0, lmaxX=0, lY=0; //left semiarea
+
+	for (int i=0; i<N; i++) {
+		if (Y[i] == 0) { //point of triangle basis
+			if (X[i] > 0) {
+				if (X[i] > rmaxX) {
+					if (rminX == 0) rminX = X[i];
+					rmaxX = X[i];
+				}
+				if (X[i] < rminX) rminX = X[i];
+			}
+			if (X[i] < 0) {
+				if (X[i] < lminX) {
+					if (lmaxX == 0) lmaxX = X[i];
+					lminX = X[i];
+				}
+				if (X[i] > lmaxX) lmaxX = X[i];
+			}
+		
+		}
+		else {
+			if (X[i] > 0) rY = (abs(Y[i])>rY) ? abs(Y[i]) : rY;
+			if (X[i] < 0) lY = (abs(Y[i])>lY) ? abs(Y[i]) : lY;
+		}		
+	}
+
+	float S = (0.5*(rmaxX-rminX)*rY > 0.5*(lmaxX-lminX)*lY) ? 0.5*(rmaxX-rminX)*rY : 0.5*(lmaxX-lminX)*lY; 
+	
+	cout << S;
+	return 0;
+}
