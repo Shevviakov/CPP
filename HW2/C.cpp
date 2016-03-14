3a91a4bd706076c2b9b3c058eab04a00e4aebd19
Dima <shedi26@mail.ru>
2016-03-14 19:58:05 +0300

    HW2 Task C

--- HW2/C.cpp
+++ HW2/C.cpp
@@ -8,3 +8,30 @@
 	Формат результата
 4
 */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int N1, N2, count=0, i=0, j=0;
+	
+	cin >> N1 >> N2;
+
+	int arr1 [N1], arr2 [N2];
+
+	for (int i=0; i<N1; i++) cin >> arr1[i];
+	for (int i=0; i<N2; i++) cin >> arr2[i];
+	
+	while (i<N1 && j<N2) {
+		if (arr1[i] == arr2[j]) {count++; i++; j++;}
+		if (arr1[i] > arr2[j]) j++;
+		if (arr1[i] < arr2[j]) i++;
+	}
+
+	cout << count;
+	
+	return 0;
+}
