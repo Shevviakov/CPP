fd3f743a0c80ddfb77c542f6e21164a1ef4da337
Dima <shedi26@mail.ru>
2016-03-18 21:15:45 +0300

    wrong HW1 Task D code

--- HW1/D.cpp
+++ HW1/D.cpp
@@ -13,3 +13,58 @@ x0 y0 – такое решение уравнения, что x0 минима
 2 4 5
 	No
 */
+
+
+#include <iostream>
+
+using namespace std;
+
+void eEuclid (int*arr) { //[0] = x; [1] = y; [2] = d
+	int a, b;
+
+	if (arr[0] >= arr[1]) {a = arr[0]; b = arr[1];}
+	else {b = arr[0]; a = arr[1];}
+	
+	if (b == 0) {arr[0] = 1; arr[1] = 0; arr[2] = a; return;}
+	
+	int arr2[3];
+	arr2[0] = b;
+	arr2[1] = a%b;
+	
+	eEuclid (arr2);
+	
+	arr[0] = arr2[1];
+	arr[1] = arr2[0] - arr2[1]*a/b;
+	arr[2] = arr2[2];
+	return;		
+}
+
+
+int main() {
+	
+	int a, b, n;
+
+	cin >> a;
+	cin >> b;
+	cin >> n;
+	
+	int arr[3];
+
+	arr[0] = a;
+	arr[1] = b;
+
+	eEuclid(arr);
+	
+	int c = arr[2];
+	cout << (arr[0] = a/c) << "\n";
+	cout << (arr[1] = b/c) << "\n";
+	
+	eEuclid(arr);
+	
+	
+	cout << arr[0] << "\n";
+	cout << arr[1] << "\n";
+//	cout << arr[2] << "\n";
+	
+	return 0;
+}
