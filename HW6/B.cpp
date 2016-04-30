ea7aef91dbecbf60be906ba949a1589991f0f4cc
Dima <shedi26@mail.ru>
2016-04-30 20:32:43 +0300

    HW6 task templates

---
+++ HW6/B.cpp
@@ -0,0 +1,35 @@
+/*
+	Покупка
+У покупателя имеется набор N монет A1 ≤ A2 ≤ ... ≤ AN, каждой монеты ровно по одной штуке.
+Найти наименьшую стоимость предмета, который нельзя купить, используя только эти монеты.
+	Input format
+	Output format
+	Examples
+Input	Output
+5
+1
+2
+4
+8
+16
+	32
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int N, sum = 0;
+	cin >> N;
+	
+	int coin[N];
+	for (int i=0; i<N; i++) {cin >> coin[i]; sum += coin[i];}
+	
+	int 
+	
+	
+	
+	
+	return 0;
+}
